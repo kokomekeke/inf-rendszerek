46f582d2a1f5d0dbfcdab8ed295144786572d68f
kokomekeke <72065311+kokomekeke@users.noreply.github.com>
2022-05-12 14:33:29 +0200

    Add files via upload

---
+++ 1649787132306-asd.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class asd1649787132306 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
